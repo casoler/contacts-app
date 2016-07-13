5a66fe89148d52e13130d7c4ef5ce9d5ad61e858
casoler <casoler11@gmail.com>
2016-07-12 20:09:38 -0500

    add restful routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,44 @@
 class ContactsController < ApplicationController
 
-  def all_contacts
-    @contacts = Contact.all
-    render :contacts
+  def index
+    @contacts = Contact.all.sort_by(&:last_name)
   end
 
-  def first_contact
-    @contact = Contact.first
-    render :contact
+  def new
+  end
+
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    @contact.save
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+  end
+
+  def edit
+    @contact = Contact.find_by(id: params[:id])
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+
+    @contacts = Contact.all.sort_by(&:last_name)
   end
 end
